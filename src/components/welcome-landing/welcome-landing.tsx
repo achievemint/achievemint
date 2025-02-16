480168c5e45903820fb8b3bc4e06d6a572b4f296
Lucas Combs <l-combs@hotmail.com>
2025-02-16 16:52:30 -0500

    NOGH: Remove login redirect screen

--- src/components/welcome-landing/welcome-landing.tsx
+++ src/components/welcome-landing/welcome-landing.tsx
@@ -1,36 +1,28 @@
 'use client'
 
-import {Button,} from "@mui/material";
+import {Button, CircularProgress,} from "@mui/material";
 import Link from "next/link";
-import {useSession} from "next-auth/react";
+import {SessionContextValue, signIn, useSession} from "next-auth/react";
+import React, {ReactElement} from "react";
 
 export default function Home() {
-    const session = useSession()
+    const {status, data} = useSession()
 
-    const sessionLayout = <div>
-        <ul>
-            <li>Session status: {session.status}</li>
-            <li>
-                Session data:
-                <pre>{JSON.stringify(session.data, null, 2)}</pre>
-            </li>
+    const contentStates: Record<SessionContextValue['status'], ReactElement> = {
+        'authenticated': <Link href={"/games"}><Button variant={"contained"} color={"primary"}>View game
+            list</Button></Link>,
+        'unauthenticated': <Button variant={"contained"} onClick={() => signIn("steam")}>Sign in</Button>,
+        'loading': <CircularProgress size="2rem" className="m-auto"/>
+    }
 
-            <li>
-                Session test: {session?.data?.user?.steam?.steamid}
-            </li>
-        </ul>
-    </div>
-
-    const linkToGamesPage = <Link href={"/games"}><Button variant={"contained"} color={"primary"}>View game
-        list</Button></Link>
-
+    const username = data?.user?.name;
 
     return (
         <div className="w-full h-full flex flex-col items-center justify-around">
-            <div className={"flex flex-col gap-4"}>
+            <div className={"flex flex-col gap-4 items-center"}>
                 <h1 className={"text-4xl font-bold"}>Welcome to Achievemint!</h1>
-                {sessionLayout}
-                {linkToGamesPage}
+                {username && <h2 className={"text-2xl"}>Hello, {username}!</h2>}
+                {contentStates[status]}
             </div>
         </div>
     )
