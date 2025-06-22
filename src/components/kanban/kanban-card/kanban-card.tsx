ace2cbad651401ab1ccec8c5bed0348513faf25b
Lucas Combs <l-combs@hotmail.com>
2025-06-22 14:58:45 -0400

    GH-93 Add test coverage for Kanban drag & drop

--- src/components/kanban/kanban-card/kanban-card.tsx
+++ src/components/kanban/kanban-card/kanban-card.tsx
@@ -5,7 +5,7 @@ import AchievementCard from "@/components/achievement-card/achievement-card";
 export default function KanbanCard({achievement}: {achievement: Achievement}): React.ReactElement {
     const { attributes, listeners, setNodeRef, transform} = useDraggable({ id: achievement.name});
 
-    return <div ref={setNodeRef} {...attributes} {...listeners} className={transform ? 'opacity-50' : ''}>
+    return <div ref={setNodeRef} {...attributes} {...listeners} data-test={'draggable-card'} className={transform ? 'opacity-50' : ''}>
         <AchievementCard  achievement={achievement}/>
     </div>
 }
