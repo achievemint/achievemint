52f0353bf4607591fbd869b0dc2414766d51e1d0
tiemonl <github.lg4iy@simplelogin.fr>
2025-12-23 16:11:32 -0500

    Update `getGames` to include total game count and integrate stats into the welcome page

--- src/services/getGames.ts
+++ src/services/getGames.ts
@@ -1,8 +1,13 @@
 import {Game} from "@/entities/Game";
 import {Session} from "next-auth";
 
-export default async function getGames(session: Session): Promise<Array<Game>> {
+export interface GamesResponse {
+    games: Array<Game>;
+    gameCount: number;
+}
+
+export default async function getGames(session: Session): Promise<GamesResponse> {
     const steamId = session?.user?.steam?.steamid
     const response = await fetch(`/api/games?&steamid=${steamId}`);
-    return (await response.json()).games;
+    return await response.json();
 }
