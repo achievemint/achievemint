1101e51b3367f3a18ef0d2e2efbb4609c97952c6
Lucas Combs <l-combs@hotmail.com>
2025-12-31 15:04:35 -0500

    GH-30: Resolve styling issues + mobile friendly landscape

--- src/components/achievement-card/achievement-card.tsx
+++ src/components/achievement-card/achievement-card.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {Achievement} from "@/entities/Achievement";
 import Image from "next/image";
+import classNames from "classnames";
 
 interface AchievementCardProps {
     achievement: Achievement;
 }
 
-function UnlockDate (args:{date: number, unlocked: boolean}) {
+function UnlockDate(args: { date: number, unlocked: boolean }) {
     if (args.unlocked) {
         // date is in UTC epoch seconds
         const localDateTime: Date = new Date(0);
         localDateTime.setUTCSeconds(args.date);
 
-        const formattedDate =  new Intl.DateTimeFormat("en-US", {
+        const formattedDate = new Intl.DateTimeFormat("en-US", {
             year: "numeric",
             month: "2-digit",
             day: "2-digit",
@@ -24,22 +25,38 @@ function UnlockDate (args:{date: number, unlocked: boolean}) {
     }
 }
 
-export default function AchievementCard({ achievement: {unlocked, icon, iconGray, name, description, unlockDate} }: AchievementCardProps): React.ReactElement {
+export default function AchievementCard({
+                                            achievement: {
+                                                unlocked,
+                                                icon,
+                                                iconGray,
+                                                name,
+                                                description,
+                                                unlockDate
+                                            }
+                                        }: AchievementCardProps): React.ReactElement {
     const backgroundColor = unlocked ? 'bg-green-500' : 'bg-gray-500';
+
     return (
-        <div className={"border-4 flex rounded-md border-blue-400 p-1 " + backgroundColor} data-test={"achievement-card"}>
-            <div className={"relative w-12 h-12"}>
-                <Image fill={true}
-                       src={unlocked ? icon : iconGray}
-                       alt={"achievement icon"}/>
-            </div>
-            <div className={"flex-col pb-2 pl-1 grow"}>
-                <h3 className="font-bold">{name}</h3>
-                <p className="text-xs">{description}</p>
-            </div>
-            <div className={"flex-col pl-2"}>
-                <UnlockDate date={unlockDate}
-                            unlocked={unlocked}/>
+        <div
+            className={classNames("border-4 select-none flex flex-col rounded-md border-blue-400 p-1", backgroundColor)}
+            data-test={"achievement-card"}>
+            <div className={"flex flex-row"}>
+                <div className={"relative w-12 h-12 aspect-square"}>
+                    <Image fill={true}
+                           src={unlocked ? icon : iconGray}
+                           alt={"achievement icon"}/>
+                </div>
+                <div className={"flex-col lg:pb-2 pl-1 grow"}>
+                    <h3 className="font-bold lg:text-lg sm:text-xs">{name}</h3>
+                    <p className="text-xs">{description}</p>
+                </div>
+                <div className={classNames("flex-col pl-2 text-center", {
+                    "hidden": !unlocked
+                })}>
+                    <UnlockDate date={unlockDate}
+                                unlocked={unlocked}/>
+                </div>
             </div>
         </div>
     );
--- src/components/kanban/kanban-board/kanban-board.tsx
+++ src/components/kanban/kanban-board/kanban-board.tsx
@@ -58,7 +58,8 @@ export default function KanbanBoard({achievements, gameId}: {
     }
 
     return (
-        <div className={"flex justify-around max-h-full w-full gap-1 lg:gap-20 lg:ml-20 lg:mr-20"}>
+        <div
+            className={"flex justify-around max-h-full w-full landscape:max-lg:max-w-full gap-1 lg:gap-20 lg:ml-20 lg:mr-20"}>
             <DndContext onDragEnd={handleDragEnd} onDragStart={handleDragStart} sensors={[
                 KeyboardSensor, MouseSensor, PointerSensor, TouchSensor
             ].map(sensor => {
